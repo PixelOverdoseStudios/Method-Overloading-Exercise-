5072e9601f7d8baf4a5369217620b8e895c9b764
dustin wells <blackxangel92@gmail.com>
2024-11-01 11:27:16 -0500

    Finished Program

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //example using the methods
+            Console.WriteLine(Add(2, 4, true));
+        }
+
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        static string Add(int num1, int num2, bool inDollars)
+        {
+            int sum = num1 + num2;
+
+            if (inDollars)
+                return sum == 1 ? $"{sum} dollar" : $"{sum} dollars";
+            else
+                return "No instructions given if boolean is false.";
         }
     }
 }
